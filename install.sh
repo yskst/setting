eba31744b55566807ea6da996db5e592d806bfcc
yousuke <you@yosukes-Mac-Book.local>
2016-04-21 00:33:27 +0900

    Setting neobundle for manegement vim plugin.

--- install.sh
+++ install.sh
@@ -6,3 +6,6 @@ scpdir=`dirname $0`
 dotfs=`find $scpdir -name '.*rc' -a -type f`
 
 cp $cpflags $dotfs ~/
+
+echo "Set up neobundle"
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
